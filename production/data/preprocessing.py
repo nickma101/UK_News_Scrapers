d2395220c66c8fa0b87bba923c44652f1ae90b2c
nickma101 <nimima95@gmail.com>
2023-06-15 18:28:03 +0200

    to-do list for preprocessing

--- production/data/preprocessing.py
+++ production/data/preprocessing.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 import json
 
-#setting the current date
-now = datetime.utcnow()
-date = now.strftime('%Y-%m-%d')
+#what to include / exclude
+topics = []
+min-length = 500
+date = datetime.utcnow().strftime('%Y-%m-%d')
+
 
 #retrieveing the daily data
 def get_data():
@@ -16,13 +18,20 @@ def get_data():
             articles = json.load(data)
         except:
             print('no articles today it seems')
+    for article in articles:
+        #some preprocessing, including:
+        # standardising datetimes
+        # standardising topics
     return articles
 
 def get_articles():
     data = get_data()
     articles = []
     for d in data:
-        #some preprocessing
+        #some filtering, including:
+        # filtering out short texts
+        # filtering out old articles
+        # filtering out unwanted topics
         articles.append(d)
     return articles
 
