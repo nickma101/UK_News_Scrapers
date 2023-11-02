14953e3e90e8168eaab38d7bc338f972d9b705f9
nickma101 <nimima95@gmail.com>
2023-11-02 14:37:04 +0100

    fixed evening standard scraper, should be good to go

--- production/standard_informfully.py
+++ production/standard_informfully.py
@@ -31,12 +31,17 @@ def get_rss_feed(feed):
         article_props['title'] = rss_article.title
         article_props['lead'] = rss_article.summary
         article_props['author'] = rss_article.author
-        article_props['primaryCategory'] = rss_article.tags
+        try:
+            article_props['primaryCategory'] = rss_article.tags
+        except:
+            article_props['primaryCategory'] = "None"
         datestring = rss_article.published.split(" GMT")[0]
         published = parser.parse(datestring)
         article_props['date_published'] = published
-        article_props['image'] = rss_article.media_content
-        #article_props['image'] = "None"
+        try:
+            article_props['image'] = rss_article.media_content
+        except:
+            article_props['image'] = "None"
         article_props['date_updated'] = rss_article.updated
 
         article_list.append(article_props)
@@ -49,26 +54,27 @@ def scrape_article(article):
     response = requests.get(article['url'])
     soup = BeautifulSoup(response.content, 'html.parser')
     # scrape article text
-    divs = soup.find('div', {'class': 'sc-evdWiO iHBFwX'})
+    divs = soup.find('div', {'class': 'sc-enDNfw iLaNIc'})
     paragraphs = []
-    first_letter = divs.find('span').text
+    #first_letter = divs.find('span').text
     for div in divs:
-        block = div.find_all('p')
+        block = div.find_all(['p', 'h2'])
         paragraphs.extend(block)
     body = []
-    first_paragraph = paragraphs[0]
-    body.append({"type": "text", "text": str(first_letter) + str(first_paragraph.text)})
-    other_paragraphs = paragraphs[1:]
-    for p in other_paragraphs:
+    #first_paragraph = paragraphs[0]
+    #body.append({"type": "text", "text": str(first_letter) + str(first_paragraph.text)})
+    #other_paragraphs = paragraphs[1:]
+    for p in paragraphs:
         if ("Read more:" not in p.text and
                 "Sign up for exclusive newsletters" not in p.text and
                 "By clicking Sign up you confirm that" not in p.text and
                 "This site is protected by reCAPTCHA" not in p.text):
-            if p.find('strong'):
-                body.append({"type": "headline", "text": str(p.text)})
+            if p.name == 'h2':
+                cleaned_text = p.get_text().replace('"', "'")
+                body.append({"type": "headline", "text": cleaned_text})
             else:
-                text = str(p.text).replace('The Standard', 'Informfully')
-                body.append({"type": "text", "text": text})
+                cleaned_text = str(p.text).replace('The Standard', 'Informfully').replace('"', "'")
+                body.append({"type": "text", "text": cleaned_text})
     # scrape category
     category = article['primaryCategory'][0]['term']
     # rename categories
@@ -101,7 +107,7 @@ def scrape_article(article):
     # create article
     document = create_article(
         url=article['url'],                                         # string
-        primary_category=category,                                  # string
+        primary_category=category,     # string
         sub_categories="None",                                      # string
         title=article['title'],                                     # string
         lead=remove_html_tags(article['lead']),                     # string
@@ -121,6 +127,7 @@ def scrape():
     newsarticles_collection = []  # Collection to store complete articles
     retrieved_articles = 0
     skipped_articles = 0
+
     for feed in NEWS_FEEDS:
         rss_results = get_rss_feed(feed)  # Get partial article information from RSS feeds
         for article in rss_results:
@@ -134,16 +141,19 @@ def scrape():
                 print(f"Couldn't scrape article: {article['url']}")
                 print(e)
                 skipped_articles += 1
-    print(retrieved_articles, skipped_articles)
-    dateString = str(date)[:10]
-    filename = "standard_articles" + dateString + ".json"
-    desired_dir = "data"
-    full_path = os.path.join(desired_dir, filename)
 
-    with open(full_path, "w") as file:
-        json.dump(newsarticles_collection, file, default=str)
+    print(retrieved_articles, skipped_articles)
+
+
+#    dateString = str(date)[:10]
+#    filename = "standard_articles" + dateString + ".json"
+#    desired_dir = "data"
+#    full_path = os.path.join(desired_dir, filename)
+
+#    with open(full_path, "w") as file:
+#        json.dump(newsarticles_collection, file, default=str, ensure_ascii=False)
 
     return newsarticles_collection
 
 
-scrape()
+#scrape()
